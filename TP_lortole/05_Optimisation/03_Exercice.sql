f0411b4220904bd469784afb6d987c4c63df68a7
HighX97 <loic.ortole@gmail.com>
2016-11-25 16:04:35 +0100

    TP Optimisation de requˆetes dans le contexte d’Oracle : Exercice 3

---
+++ TP_lortole/05_Optimisation/03_Exercice.sql
@@ -0,0 +1,34 @@
+/*
+Exercice 3
+*/
+
+
+/*
+1. Exprimez la requˆete donnez le nom, la latitude et la longitude des communes qui se situent
+dans les d ́epartements de l’H ́erault et du Gard sous diff ́erentes formes (expression d’une semi-
+jointure) : sous forme de jointure puis sous forme de requˆete imbriqu ́ee (test de vacuit ́e (exists)
+et test d’appartenance (in)). Quelle est l’ ́ecriture qui vous semble la moins coˆ
+uteuse (utilisez set
+timing et explain ) ? Quels sont les op ́erateurs physiques exploit ́es respectivement pour exprimer
+la jointure ? Exploiter les directives (hint) pour forcer le choix d’un op ́erateur (par exemple
+use nl). Construisez sur papier un arbre alg ́ebrique puis les plans physiques correspondant `
+a
+chaque plan d’ex ́ecution choisi.
+
+
+*/
+
+/*
+2. Ecrivez la requˆete donnez le nom et la population 2010 des communes qui ont plus d’habitants
+que le nombre moyen d’habitants par commune. Exploitez le plan d’ex ́ecution pour cette requˆete.
+Quelles sont les op ́erations exploit ́ees par l’optimiseur ?
+*/
+
+
+/*
+3. Ecrivez la requˆete de diff ́erentes mani`eres : donnez le nom des communes, le nom de leur
+d ́epartement et de leur r ́egion respectifs lorsque ces communes sont situ ́ees dans les r ́egions Midi-
+Pyr ́en ́ees, Languedoc-Roussillon et Provence-Alpes-Cˆ
+ote d’Azur. Commentez les plans d’ex ́ecution
+obtenus.
+*/
