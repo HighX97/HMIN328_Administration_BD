fd2b2195ca127feca4bf7725b3c168cfa7b73d5b
HighX97 <loic.ortole@gmail.com>
2016-11-25 14:55:31 +0100

    TP Optimisation de requˆetes dans le contexte d’Oracle : Exercice 1

--- TP_lortole/05_Optimisation/01_Exercice.sql
+++ TP_lortole/05_Optimisation/01_Exercice.sql
@@ -1,11 +1,9 @@
 /*
-Vous reprendrez les questions pos ́ees lors du dernier TP concernant le recours `a l'index 1 dans
+Vous reprendrez les questions pos ́ees lors du dernier TP concernant le recours à l'index 1 dans
 une s ́erie de requˆetes SQL sur la table Commune. Vous afficherez les plans propos ́es par l'optimiseur
 pour chacune de ces questions et vous analyserez les r ́esultats obtenus. Vous pouvez aussi rapidement
 dessiner les plans physiques destin ́es `
-a l'ex ́ecution des requˆetes. Dans un second temps, soit vous
-d ́esactiverez la contrainte de cl ́e primaire sur code_insee qui conduit `a la suppression de l'index, soit
-vous exploiterez la directive NO INDEX et vous r ́e ́evaluerez les plans pr ́ec ́edents.
+a l'ex ́ecution des requˆetes.
 */
 
 /*1*/
@@ -238,5 +236,422 @@ PLAN_TABLE_OUTPUT
 	      OR "CODE_INSEE"='09334')
 
 16 rows selected.
+
+*/
+
+
+/*
+Dans un second temps:
+soit vous desactiverez la contrainte de cle primaire sur code_insee qui conduit à la suppression de l'index
+*/
+
+ALTER TABLE commune
+DISABLE CONSTRAINT pk_Commune;
+
+/*1*/
+
+explain plan for select code_insee from commune ;
+select plan_table_output from table(dbms_xplan.display()) ;
+/*
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    | 36318 |	177K|	582   (1)| 00:00:07 |
+|   1 |  TABLE ACCESS FULL| COMMUNE | 36318 |	177K|	582   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+8 rows selected.
+
+
+
+*/
+
+/*2*/
+
+explain plan for select nom_Com from commune ;
+select plan_table_output from table(dbms_xplan.display()) ;
+/*
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    | 36318 |	425K|	581   (1)| 00:00:07 |
+|   1 |  TABLE ACCESS FULL| COMMUNE | 36318 |	425K|	581   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+8 rows selected.
+
+
+*/
+
+/*3*/
+
+explain plan for select nom_Com from commune where code_insee='34192' ;
+select plan_table_output from table(dbms_xplan.display()) ;
+/*
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    |	  1 |	 17 |	582   (1)| 00:00:07 |
+|*  1 |  TABLE ACCESS FULL| COMMUNE |	  1 |	 17 |	582   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+---------------------------------------------------
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+
+   1 - filter("CODE_INSEE"='34192')
+
+13 rows selected.
+
+
+*/
+
+/*4*/
+
+explain plan for select nom_Com from commune where code_insee like '34%' ;
+select plan_table_output from table(dbms_xplan.display()) ;
+/*
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    |	 17 |	289 |	582   (1)| 00:00:07 |
+|*  1 |  TABLE ACCESS FULL| COMMUNE |	 17 |	289 |	582   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+---------------------------------------------------
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+
+   1 - filter("CODE_INSEE" LIKE '34%')
+
+13 rows selected.
+
+
+*/
+
+/*5*/
+
+explain plan for select nom_Com from commune where code_insee like '%392' ;
+select plan_table_output from table(dbms_xplan.display()) ;
+/*
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    |  1816 | 30872 |	582   (1)| 00:00:07 |
+|*  1 |  TABLE ACCESS FULL| COMMUNE |  1816 | 30872 |	582   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+---------------------------------------------------
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+
+   1 - filter("CODE_INSEE" LIKE '%392' AND "CODE_INSEE" IS NOT NULL)
+
+13 rows selected.
+
+
+*/
+
+/*6*/
+
+explain plan for select nom_Com from commune where code_insee >= 34 ;
+select plan_table_output from table(dbms_xplan.display()) ;
+/*
+
+LAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    |  1816 | 30872 |	582   (1)| 00:00:07 |
+|*  1 |  TABLE ACCESS FULL| COMMUNE |  1816 | 30872 |	582   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+---------------------------------------------------
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+
+   1 - filter(TO_NUMBER("CODE_INSEE")>=34)
+
+13 rows selected.
+
+*/
+
+/*7*/
+
+explain plan for select nom_Com from commune where code_insee in ('09330','09331','09332','09334');
+select plan_table_output from table(dbms_xplan.display()) ;
+/*
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    |	  4 |	 68 |	582   (1)| 00:00:07 |
+|*  1 |  TABLE ACCESS FULL| COMMUNE |	  4 |	 68 |	582   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+---------------------------------------------------
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+
+   1 - filter("CODE_INSEE"='09330' OR "CODE_INSEE"='09331' OR
+	      "CODE_INSEE"='09332' OR "CODE_INSEE"='09334')
+
+*/
+
+ALTER TABLE commune
+ENABLE CONSTRAINT pk_Commune;
+
+
+/*
+Dans un second temps:
+soit vous desactiverez la contrainte de cle primaire sur code_insee qui conduit à la suppression de l'index
+*/
+
+/*1*/
+
+explain plan for select /*+ NO_INDEX(Commune) */  code_insee from commune ;
+select plan_table_output from table(dbms_xplan.display()) ;
+
+explain plan for select * from commune ;
+select plan_table_output from table(dbms_xplan.display()) ;
+
+explain plan for select code_insee from commune ;
+select plan_table_output from table(dbms_xplan.display()) ;
+
+
+/*
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    | 36318 |	177K|	582   (1)| 00:00:07 |
+|   1 |  TABLE ACCESS FULL| COMMUNE | 36318 |	177K|	582   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+8 rows selected.
+
+*/
+
+/*2*/
+
+explain plan for select /*+ NO_INDEX(Commune) */  nom_Com from commune ;
+select plan_table_output from table(dbms_xplan.display()) ;
+/*
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    | 36318 |	425K|	581   (1)| 00:00:07 |
+|   1 |  TABLE ACCESS FULL| COMMUNE | 36318 |	425K|	581   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+8 rows selected.
+
+
+*/
+/*3*/
+
+explain plan for select /*+ NO_INDEX(Commune) */  nom_Com from commune where code_insee='34192' ;
+select plan_table_output from table(dbms_xplan.display()) ;
+/*
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    |	  1 |	 17 |	582   (1)| 00:00:07 |
+|*  1 |  TABLE ACCESS FULL| COMMUNE |	  1 |	 17 |	582   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+---------------------------------------------------
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+
+   1 - filter("CODE_INSEE"='34192')
+
+13 rows selected.
+
+*/
+/*4*/
+
+explain plan for select /*+ NO_INDEX(Commune) */  nom_Com from commune where code_insee like '34%' ;
+select plan_table_output from table(dbms_xplan.display()) ;
+/*
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    |	 17 |	289 |	582   (1)| 00:00:07 |
+|*  1 |  TABLE ACCESS FULL| COMMUNE |	 17 |	289 |	582   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+---------------------------------------------------
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+
+   1 - filter("CODE_INSEE" LIKE '34%')
+
+13 rows selected.
+
+
+*/
+/*5*/
+
+explain plan for select /*+ NO_INDEX(Commune) */  nom_Com from commune where code_insee like '%392' ;
+select plan_table_output from table(dbms_xplan.display()) ;
+/*
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    |  1816 | 30872 |	582   (1)| 00:00:07 |
+|*  1 |  TABLE ACCESS FULL| COMMUNE |  1816 | 30872 |	582   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+---------------------------------------------------
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+
+   1 - filter("CODE_INSEE" LIKE '%392')
+
+13 rows selected.
+
+
+*/
+/*6*/
+
+explain plan for select /*+ NO_INDEX(Commune) */  nom_Com from commune where code_insee >= 34 ;
+select plan_table_output from table(dbms_xplan.display()) ;
+/*
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    |  1816 | 30872 |	582   (1)| 00:00:07 |
+|*  1 |  TABLE ACCESS FULL| COMMUNE |  1816 | 30872 |	582   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+---------------------------------------------------
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+
+   1 - filter(TO_NUMBER("CODE_INSEE")>=34)
+
+13 rows selected.
+
+
+*/
+/*7*/
+
+explain plan for select /*+ NO_INDEX(Commune) */  nom_Com from commune where code_insee in ('09330','09331','09332','09334');
+select plan_table_output from table(dbms_xplan.display()) ;
+/*
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+Plan hash value: 1326938554
+
+-----------------------------------------------------------------------------
+| Id  | Operation	  | Name    | Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |	    |	  4 |	 68 |	582   (1)| 00:00:07 |
+|*  1 |  TABLE ACCESS FULL| COMMUNE |	  4 |	 68 |	582   (1)| 00:00:07 |
+-----------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+---------------------------------------------------
+
+PLAN_TABLE_OUTPUT
+--------------------------------------------------------------------------------
+
+   1 - filter("CODE_INSEE"='09330' OR "CODE_INSEE"='09331' OR
+	      "CODE_INSEE"='09332' OR "CODE_INSEE"='09334')
+
+14 rows selected.
+
+*/
+
+
+/*
+
+Pour r ́epondre au mieux `
+a ces questions, construisez un tableau sur papier, contenant les informations :
+num ́
+ero requˆ
+ete recours `
+a l’index (o/n) raison du choix de l’optimiseur op ́
+erateurs mis en jeu
 
 */
