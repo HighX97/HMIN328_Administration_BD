0e40fce8426be1a74cad5e5ab4b17be81605f2ec
HighX97 <loic.ortole@gmail.com>
2016-12-09 13:30:51 +0100

    TP Migration

---
+++ TP_lortole/07_Migration/PrintCols.sql
@@ -0,0 +1,26 @@
+
+create or replace PROCEDURE print_columns (tab_name in VARCHAR) IS
+   TYPE tabColTyp IS REF CURSOR;  -- define weak REF CURSOR type
+   tv_cv   tabColTyp;  -- declare cursor variable
+   col_name VARCHAR2(30);
+  TYPE  columns_t IS TABLE OF VARCHAR2 (30) INDEX BY BINARY_INTEGER ;
+   t_columns columns_t;
+  i pls_integer :=0 ;
+BEGIN
+   OPEN tv_cv FOR  -- open cursor variable
+      'SELECT  column_name  FROM user_tab_columns
+                    WHERE table_name = '''||  tab_name || '''';
+LOOP
+   FETCH tv_cv INTO col_name ;  -- fetch next row
+   EXIT WHEN tv_cv%NOTFOUND;  -- exit loop when last row is fetched
+   DBMS_OUTPUT.PUT_LINE('Record: ' || col_name);
+   t_columns(i+1) := col_name;
+   DBMS_OUTPUT.PUT_LINE('Record: ' || t_columns(i+1));
+   i:=i+1;
+END LOOP;
+      CLOSE tv_cv;
+END;
+/
+
+exec print_columns ('EMP')
+
